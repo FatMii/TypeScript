095165b8a0f46a49878ae0a4947e88e3bd95fd73
FatMii <ot.lzq@163.com>
2023-07-29 00:01:17 +0800

    update

--- ts/9.工具类型.ts
+++ ts/9.工具类型.ts
@@ -19,16 +19,34 @@ const user2: Required<Person2> = {
 //   name:'树哥'
 // } // 编译正确
 
+/**
+ * 原理
+ * Make all properties in T optional
+ */
+//  type Partial<T> = {
+//   [P in keyof T]?: T[P]
+// }
+
 //3.Exclude
 //Exclude<T, U> 的作用是将某个类型中属于另一个的类型移除掉,剩余的属性构成新的类型
 // type T0 = Exclude<"a" | "b" | "c", "a">; // "b" | "c"
 // type T1 = Exclude<"a" | "b" | "c", "a" | "b">; // "c"
 // type T2 = Exclude<string | number | (() => void), Function>; // string | number
+/**
+ * 原理
+ * Exclude from T those types that are assignable to U
+ */
+//  type Exclude<T, U> = T extends U ? never : T
 
 //4.Extract
 //和 Exclude 相反，Extract<T,U> 从 T 中提取出 U。
 // type T0 = Extract<"a" | "b" | "c", "a" | "f">; // "a"
 // type T1 = Extract<string | number | (() => void), Function>; // () =>void
+/**
+ * 原理
+ * Extract from T those types that are assignable to U
+ */
+//  type Extract<T, U> = T extends U ? T : never
 
 //5.Readonly
 //把数组或对象的所有属性值转换为只读的，这就意味着这些属性不能被重新赋值。
@@ -36,6 +54,12 @@ const user2: Required<Person2> = {
 //   name: string;
 //   age: number;
 //   gender?: "male" | "female";
+// }
+/**
+ * 原理
+ */
+//  type Readonly<T> = {
+//   readonly [P in keyof T]: T[P]
 // }
 
 // let p: Readonly<Person> = {
@@ -54,6 +78,13 @@ const user2: Required<Person2> = {
 //   key1: "hello 啊",
 //   key2: "树哥",
 // };
+/**
+ * 原理
+ * Construct a type with a set of properties K of type T
+ */
+//  type Record<K extends keyof any, T> = {
+//   [P in K]: T
+// }
 
 //7.Pick 从某个类型中挑出一些属性出来
 // type Person = {
@@ -61,6 +92,13 @@ const user2: Required<Person2> = {
 //     age:number;
 //     gender:string
 //   }
+/**
+ * 原理
+ * From T, pick a set of properties whose keys are in the union K
+ */
+//  type Pick<T, K extends keyof T> = {
+//   [P in K]: T[P]
+// }
 
 //   type P1 = Pick<Person, "name" | "age">; // { name: string; age: number; }
 
@@ -80,16 +118,43 @@ const user2: Required<Person2> = {
 //     name: '树哥'
 //   }
 
+/**
+ * 原理
+ * Construct a type with the properties of T except for those in type K.
+ */
+//  type Omit<T, K extends keyof any> = 
+//  Pick<T, Exclude<keyof T, K>
+
 //9.NonNullable 去除类型中的 null 和 undefined
 // type P1 = NonNullable<string | number | undefined>; // string | number
 // type P2 = NonNullable<string[] | null | undefined>; // string[]
+
+/**
+ * 原理
+ * Exclude null and undefined from T
+ */
+//  type NonNullable<T> = 
+//  T extends null | undefined ? never : T
 
 //10.ReturnType 用来得到一个函数的返回值类型
 type Func = (value: string) => string;
 const test: ReturnType<Func> = "1";
 
+/**
+ * 原理
+ * Obtain the return type of a function type
+ */
+//  type ReturnType<T extends (...args: any) => any> = 
+//  T extends (...args: any) => infer R ? R : any
+
 //11.Parameters 用于获得函数的参数类型所组成的元组类型
 type P1 = Parameters<(a: number, b: string) => void>; // [number, string]
+/**
+ * 原理
+ * Obtain the parameters of a function type in a tuple
+ */
+//  type Parameters<T extends (...args: any) => any> = 
+//  T extends (...args: infer P) => any ? P : neve
 
 //12.InstanceType 返回构造函数类型T的实例类型
 class C {
